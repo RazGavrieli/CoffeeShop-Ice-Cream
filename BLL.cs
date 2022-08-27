6fab435e9e2d1225fb673fe80149b383e725c30a
RazGavrieli <90526270+RazGavrieli@users.noreply.github.com>
2022-08-27 17:29:24 +0300

    first commit

---
+++ BLL.cs
@@ -0,0 +1,15 @@
+using DAL;
+using DataProtocol; // dataprotocol is written by Amit
+
+namespace BLL
+{
+    public class businessLogic
+    {
+        // public List<Person> GetPeople(int mage)
+        // {
+        //     sqlAdapter DAL=new sqlAdapter();
+        //     var Result=DAL.GetPeople(mage);
+        //     return Result;
+        // }
+    }
+}
---
+++ DAL.cs
@@ -0,0 +1,48 @@
+using DataProtocol;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    public class sqlAdapater
+    {
+        public List<Person> GetPeople(int MinAge)
+        {
+            // var Result = new List<Person>();
+            try
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+
+                builder.DataSource = @"(localdb)\MSSQLLocalDB"; // INSERT HERE CORRECT SETTINGS
+                builder.InitialCatalog = "ArielDB";             // INSERT HERE CORRECT SETTINGS
+
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    connection.Open();
+
+                    // String sql = $"SELECT * FROM dbo.People WHERE Age > {MinAge}"; // INSERT SQL QUERRY HERE
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            // while (reader.Read()) // READ THE SQL ANSWER HERE
+                            // {
+                            //     var name = reader.GetString(1);
+                            //     var age = reader.GetInt32(2);
+                            //     Result.Add(new Person {Name = name,Age = age});
+                            // }
+                        }
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            return Result;
+        }
+
+        
+
+    }
+}
